5056cfc51c35542a1aa143fc41d559256622e24e
bduke-Skyline <bduke@skyline-ats.com>
2016-09-23 09:47:01 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,19 @@
+# OpenStack-mitaka (All-in-one node using single NIC) for Ubuntu 14.04LTS
+Bash script to install openstack-Mitaka in ubuntu 14.04LTS
+
+>Download the script using the below command:
+>>git clone https://github.com/bduke-Skyline/OpenStack.git
+
+>>cd OpenStack/
+
+>Script Usage: bash install.bash --ip_address [Your server Ip] --interface_name [interface name]
+
+>Example: bash install.bash --ip_address 192.168.1.172 --interface_name eth0
+
+IMPORTANT:
+  - Use this script in the fresh ubuntu 14.04LTS machine.
+  - Must have static IP configured.
+  - This script use same NIC/IP for Management/External/VM Datapath networks.
+  - Make sure that the IP you have given must have internet connectivity.
+  - The interface name must be the NIC name of above mentioned IP.
+
---
+++ install.bash
@@ -0,0 +1,37 @@
+#!/bin/bash
+#
+# Unattended/SemiAutomatted OpenStack Installer
+# 
+# OpenStack MITAKA for Ubuntu 14.04lts
+#
+#
+
+#usage: bash main_mitaka.bash --ip_address 192.168.1.172 --interface_name eth0
+
+# Execute getopt
+ARGS=$(getopt -o a:b -l "ip_address:,interface_name:" -- "$@");
+
+my_ip=$2
+INTERFACE_NAME=$4
+
+echo $my_ip  controller >> /etc/hosts
+ping controller -c 5
+
+#
+#Clone the installer source code
+#
+apt-get update
+apt-get install git -y
+git clone https://github.com/bduke-Skyline/openstack-mitaka.git
+cd openstack-mitaka/
+
+#
+#Always backup first
+#
+
+cp -r mitaka_configuration mitaka_configuration_bkp
+find ./mitaka_configuration -type f -exec sed -i -e 's/192.168.2.161/'$my_ip'/g' {} \;
+find ./mitaka_configuration -type f -exec sed -i -e 's/skyline_interface_name/'$INTERFACE_NAME'/g' {} \;
+
+#Start the main installer script
+bash main_mitaka_install.bash | tee -a /var/log/my_install_log.log
