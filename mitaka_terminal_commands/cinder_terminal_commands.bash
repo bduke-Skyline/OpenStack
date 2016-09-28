c5ff2afdc7869a6faf0eb7394c673a503d67e7f1
bduke-Skyline <bduke@skyline-ats.com>
2016-09-28 16:54:42 -0400

    Update cinder_terminal_commands.bash

--- mitaka_terminal_commands/cinder_terminal_commands.bash
+++ mitaka_terminal_commands/cinder_terminal_commands.bash
@@ -10,18 +10,19 @@
 source admin-openrc
 openstack user create --domain default --password CINDER_PASS cinder
 openstack role add --project service --user cinder admin
+
 openstack service create --name cinder --description "OpenStack Block Storage service" volume
-openstack service create --name cinderv2 --description "OpenStack Block Storage service" volumev2
 openstack endpoint create --region RegionOne volume public http://controller:8776/v1/%\(tenant_id\)s
 openstack endpoint create --region RegionOne volume internal http://controller:8776/v1/%\(tenant_id\)s
 openstack endpoint create --region RegionOne volume admin http://controller:8776/v1/%\(tenant_id\)s
 
+openstack service create --name cinderv2 --description "OpenStack Block Storage service" volumev2
 openstack endpoint create --region RegionOne volumev2 public http://controller:8776/v2/%\(tenant_id\)s
 openstack endpoint create --region RegionOne volumev2 internal http://controller:8776/v2/%\(tenant_id\)s
 openstack endpoint create --region RegionOne volumev2 admin http://controller:8776/v2/%\(tenant_id\)s
 
 
-su -s /bin/sh -c "cinder-manage db_sync" cinder
+su -s /bin/sh -c "cinder-manage db sync" cinder
 service cinder-scheduler restart
 service cinder-api restart
 rm -f /var/lib/cinder/cinder.sqlite
