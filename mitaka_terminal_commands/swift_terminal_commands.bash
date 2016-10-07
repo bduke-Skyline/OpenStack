8d5410257d0ae99e1812d6fa801cdeab58b20642
bduke-Skyline <bduke@skyline-ats.com>
2016-10-07 14:26:07 -0400

    Update swift_terminal_commands.bash

--- mitaka_terminal_commands/swift_terminal_commands.bash
+++ mitaka_terminal_commands/swift_terminal_commands.bash
@@ -11,3 +11,14 @@ mkfs.xfs /dev/vdd
 
 mkdir -p /srv/node/vdc
 mkdir -p /srv/node/vdd
+
+#Swift Controller comments
+source admin-openrc
+openstack user create --domain default --password SWIFT_PASS swift
+openstack role add --project service --user swift admin
+
+openstack service create --name swift --description "OpenStack Object Storage service" object-store
+openstack endpoint create --region RegionOne object-store public http://controller:8080/v1/AUTH_%\(tenant_id\)s
+openstack endpoint create --region RegionOne object-store internal http://controller:8080/v1/AUTH_%\(tenant_id\)s
+openstack endpoint create --region RegionOne object-store admin http://controller:8080/v1
+
